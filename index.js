d8563848ecb6572ed9c4b34dc3bd77dc4a0a3e7b
Jeffrey Brown <jeffreybrown@Jeffreys-MacBook-Pro.local>
2021-10-08 12:42:22 -0400

    Code refactoring and updates

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-// module.exports = require('./lib/textart.js');
-
-const textart = require('./lib/textart');
-const payload = {
-	"input":"ABBB",
-	"typeface":"" // default to doom //
-};
-
-textart.textart(payload, response => {
-  console.log(response);
-});
+module.exports = require('./lib/butthorn');
